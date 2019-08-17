fdb2ea2eb84913ee3254601c09898c3d60a70ebd
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-17 21:40:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,24 @@
 # My Code here....
 def map_to_negativize(source_array)
   i = 0
-  new_source_array = []
+  negativized_array = []
     if i < source_array.length 
-     source_array.each { |a| new_source_array.push(a*-1) }
+     source_array.each { |a| negativized_array.push(a*-1) }
      i += 1
-     p new_source_array
+     p negativized_array
     end
 end
 
 def map_to_no_change(source_array)
   source_array
+end
+
+def map_to_double(source_array)
+  i = 0
+  doubled_array = []
+    if i < source_array.length
+      source_array.each {|a| doubled_array.push(a*2)}
+      i += 1
+      p doubled_array
+    end
 end
