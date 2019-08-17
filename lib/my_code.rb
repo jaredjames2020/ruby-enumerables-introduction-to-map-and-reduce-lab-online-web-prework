f2dce0d8e2151a6a0da1dd5f3ec7e4fb28b31657
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-17 21:48:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,10 +33,10 @@ def map_to_square(source_array)
     end
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   total_no_starting_point = []
   total_with_starting_point = []
   i = 0
-    if i < source_array.length
+    if i < source_array.length && starting_point = 0
       source_array.each {|a| t}
 end
