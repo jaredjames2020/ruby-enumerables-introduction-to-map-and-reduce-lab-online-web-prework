350bcd2fd1a642fe68a334892282670db26e3c2e
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-17 21:16:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,43 @@
 # My Code here....
+def map_to_negativize(source_array)
+  #source_array.map { |a| p a*-1 }
+  source_array = [1,2,3,4,5]
+  i = 0
+  while i < source_array.length do 
+    new_source_array = Array.new(source_array)
+    new_source_array.each { |a| p a*-1 }
+    i += 1
+    p new_source_array
+  end
+end
+
+def map_to_no_change(source_array)
+  source_array.map {|a| p a}
+end
+
+def map_to_double(source_array)
+  source_array.map {|a| p a *2}
+end
+
+def map_to_square(source_array)
+  source_array.map {|a| p a**2}
+end
+
+def reduce_to_total(source_array, starting_point)
+  if starting_point = 0 
+  source_array.reduce(starting_point, :+)
+  else
+    
+end
+
+#def reduce_to_total(source_array, starting_point)
+#  source_array.reduce(100, :+) 
+#end
+
+def reduce_to_all_true(source_array)
+  source_array.all?
+end
+
+def reduce_to_any_true(source_array)
+  source_array.any?
+end
