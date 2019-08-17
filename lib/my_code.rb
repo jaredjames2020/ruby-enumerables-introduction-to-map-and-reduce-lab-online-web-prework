2a6ed0485555a843df1ae332e1870d25c05f4c20
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-17 21:28:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map_to_negativize(source_array)
   source_array = [1,2,3,4,5]
   i = 0
   new_source_array = []
-  if i < source_array.length do 
+  if i < source_array.length 
     source_array.each { |a| new_source_array.push(a*-1) }
     i += 1
     p new_source_array
