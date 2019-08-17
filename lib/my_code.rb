e456579c1ed9dbb319ba212654dc9d09f91ed151
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-17 21:54:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,10 +38,10 @@ def reduce_to_total(source_array, starting_point = 0)
   total_with_starting_point = []
   i = 0
   starting_point = 0
-    if i < source_array.length && starting_point == 0
+    if i < source_array && starting_point == 0
       #source_array.each {|a| total_no_starting_point()}
       print "zero"
-    elsif i < source_array.length && starting_point != 0
+    elsif i < source_array && starting_point != 0
       print "not zero"
     end
 end
